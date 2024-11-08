47e8e6d351d15beeb809482e11e6b418effc402a
DavidIncarbone <david.incarbone@gmail.com>
2024-11-08 14:26:01 +0100

    add inputs on index.html and declare variable on script.js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,14 @@
 "use strict";
 console.clear();
+
+// Variable Declar/Init
+
+let finalPrice = "";
+const priceKm = 0.21;
+let ageJunior = 18;
+let discountJunior = 20;
+let ageSenior = 65;
+let discountSenior = 40;
+
+// user KM
+
