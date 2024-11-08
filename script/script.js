64152e2782e6ffd5c895c1da0fa1ca49e3882b42
DavidIncarbone <david.incarbone@gmail.com>
2024-11-08 15:02:34 +0100

    add action to submit

--- script/script.js
+++ script/script.js
@@ -10,13 +10,37 @@ let discountJunior = 20;
 let ageSenior = 65;
 let discountSenior = 40;
 
-//get elements by index.html
+//get elements by initial form
 
 const userKM = document.getElementById("userKM");
 console.dir(userKM);
 const userAge = document.getElementById("userAge");
 console.dir(userAge);
-const button = document.getElementById("confirm");
-console.dir(button);
+const principalForm = document.getElementById("principalForm");
+console.dir(principalForm);
+// const button = document.getElementById("confirm");
+// console.dir(button);
+
+//get elements by result
+
+const ticketResult = document.querySelector(".d-none");
+console.dir(ticketResult);
+const ticketPrice = document.getElementById("ticketPrice");
+console.dir(ticketPrice);
+
+// Action on submit
+
+principalForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(userKM.value);
+    console.log(userAge.value);
+    ticketResult.classList.remove("d-none");
+})
+
+
+
+
+
+
 
 
